8e2b293cc02eef4595350e92da26fb763ac2f6f1
Emily Renegar <egrenegar@gmail.com>
2020-04-29 21:56:52 -0400

    initial commit to add instructor-given files

---
+++ Develop/script.js
@@ -0,0 +1,18 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+// Creates an array of the user's options to include in their password
+var pwChoices = ["lowercase", "uppercase", "numeric", "sp-char"]
